fef3bde4946c89ef40332b6d67912839503edf92
kunaaalrao <84015751+kunaaalrao@users.noreply.github.com>
2022-01-31 18:25:53 +0530

    Create print-increasing-decreasing.java

---
+++ Recursion And Backtracking/Introduction To Recursion/print-increasing-decreasing.java
@@ -0,0 +1,24 @@
+//https://nados.io/question/print-increasing-decreasing
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        pdi(n);
+    }
+    
+    public static void pdi(int n){
+        if(n == 0)
+            return;
+
+        System.out.println(n);
+        pdi(n-1);
+        System.out.println(n);
+    }
+
+}
