20f20dd9e11908e9452a0a720d0aeab9f7750670
kunaaalrao <84015751+kunaaalrao@users.noreply.github.com>
2022-01-31 18:30:07 +0530

    Create print-zigzag.java

---
+++ Recursion And Backtracking/Introduction To Recursion/print-zigzag.java
@@ -0,0 +1,26 @@
+//https://nados.io/question/print-zigzag
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        pzz(n);
+    }
+
+    public static void pzz(int n){
+        if(n == 0)
+            return;
+
+        System.out.print(n + " ");
+        pzz(n-1);
+        System.out.print(n+" ");
+        pzz(n-1);
+        System.out.print(n+" ");
+    }
+
+}
